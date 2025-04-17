9e4392eb72fbdf1dbd379f015e8fc030b4cd6221
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-17 18:27:14 -0300

    Lesson 06: add Sidebar component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author='Pedro Carvalho'
-        content='“Nutra sua mente com grandes pensamentos, pois você nunca irá mais alto do que o que você pensa.”'
-      />
-      <Post
-        author='Gabriel Meireles'
-        content='Que reflexão maravilhosa… tenho refletido muito sobre isso, em momentos difíceis tenho orado mais pedindo a Deus sabedoria para não reclamar, e tentar com o auxílio do Espírito Santo tentar entender o propósito daquela situação!'
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author='Pedro Carvalho'
+            content='“Nutra sua mente com grandes pensamentos, pois você nunca irá mais alto do que o que você pensa.”'
+          />
+          <Post
+            author='Gabriel Meireles'
+            content='Que reflexão maravilhosa… tenho refletido muito sobre isso, em momentos difíceis tenho orado mais pedindo a Deus sabedoria para não reclamar, e tentar com o auxílio do Espírito Santo tentar entender o propósito daquela situação!'
+          />
+        </main>
+      </div>
     </div>
   )
 }
