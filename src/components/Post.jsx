33872d4b6768df5659a9492967cf580b113eb1fc
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-18 18:05:34 -0300

    Lesson 08: Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src='https://media.licdn.com/dms/image/v2/C4E03AQEgjZifrHUBfQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1617150701815?e=1750291200&v=beta&t=0r07TFh3RTLfrOR0IwMovsLBkCdWaVh-baLhOK7i_kk'
+          />
+          <div className={styles.authorInfo}>
+            <strong>Pedro Carvalho</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title='18 de Abril às 17:53' dateTime='2025-04-18 17:53:30'>
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento
+          da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href='#'>jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href='#'>#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
