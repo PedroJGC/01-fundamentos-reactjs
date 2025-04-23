f3c48a86030d8ef275a466322dd32e0364682823
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-04-23 18:07:42 -0300

    Lesson 17: useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { format, formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { useState } from 'react'
 
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
@@ -7,6 +8,9 @@ import { Avatar } from './Avatar'
 import styles from './Post.module.css'
 
 export function Post({ author, publishedAt, content }) {
+  // estado = variáveis que eu quero que o componente monitore.
+  const [comments, setComments] = useState([1, 2])
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -15,6 +19,13 @@ export function Post({ author, publishedAt, content }) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment(event) {
+    event.preventDefault()
+
+    // imutabilidade
+    setComments([...comments, comments.length + 1])
+  }
 
   return (
     <article className={styles.post}>
@@ -46,7 +57,7 @@ export function Post({ author, publishedAt, content }) {
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea placeholder='Deixe um comentário' />
@@ -57,9 +68,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment />
+        })}
       </div>
     </article>
   )
